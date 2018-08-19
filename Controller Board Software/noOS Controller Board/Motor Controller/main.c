f495d3e4eab2b4f216c66d72a6af40728912ee33
Marc <42499367+Marc-Spieler@users.noreply.github.com>
2018-08-19 11:54:59 +0200

    Initial Version
    Updated Folder Structure and created Contoller Board Project

---
+++ Controller Board Software/noOS Controller Board/Motor Controller/main.c
@@ -0,0 +1,21 @@
+/*
+ * Motor Controller.c
+ *
+ * Created: 19.08.2018 11:52:27
+ * Author : Marc_Desktop
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
---
+++ Controller Board Software/noOS Controller Board/Sensor Controller/main.c
@@ -0,0 +1,21 @@
+/*
+ * Sensor Controller.c
+ *
+ * Created: 19.08.2018 11:53:08
+ * Author : Marc_Desktop
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
