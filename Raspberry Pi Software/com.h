236bf1dca81c081684eaf3fc72c69cc403fcc482
friederlontzek <48617603+friederlontzek@users.noreply.github.com>
2019-06-01 01:52:10 +0200

    Revert "fixed errors"
    This reverts commit 3a258be41d8e0187cd49f8e543c589d767285d84.

--- Raspberry Pi Software/com.h
+++ Raspberry Pi Software/com.h
@@ -4,17 +4,3 @@ extern struct Info infoBall;
 extern struct Info infoGoal;
 extern volatile int comBallReady;
 extern volatile int comGoalReady;
-
-extern int tml_stat;
-extern int tll1_stat;
-extern int tll2_stat;
-extern int trl1_stat;
-extern int trl2_stat;
-
-extern int tmu_stat;
-extern int tlu1_stat;
-extern int tlu2_stat;
-extern int tru1_stat;
-extern int tru2_stat;
-
-
