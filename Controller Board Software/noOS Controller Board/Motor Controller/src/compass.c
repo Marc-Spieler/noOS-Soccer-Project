18a719ee8bc05b70cfeca6bae8d85fb24b9e29d7
Marc Spieler <noos.marc@gmail.com>
2019-09-15 13:20:46 +0200

    activated lcd and compass in programm

--- Controller Board Software/noOS Controller Board/Motor Controller/src/compass.c
+++ Controller Board Software/noOS Controller Board/Motor Controller/src/compass.c
@@ -9,7 +9,7 @@
 #include "timing.h"
 #include "comm.h"
 
-static Bool compass_in_use = false;
+static Bool compass_in_use = true;
 uint16_t direction;
 uint16_t opponent_goal;
 Bool update_pid_compass = false;
